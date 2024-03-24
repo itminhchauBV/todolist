b344c189c7c37075cb7097b30b9be8d138f34ccd
itminhchau <daominhchau18@gmail.com>
2024-03-24 19:48:21 +0700

    final

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,39 +1,38 @@
-import './style.scss'
-import imageDown from '../assets/image/down.png'
+import { useState } from 'react'
 import imageDelete from '../assets/image/delete.png'
 import imageEdit from '../assets/image/pencil.png'
-import { useState } from 'react'
+import './style.scss'
 import { useAddPostMutation, useDeletePostMutation, useGetPostsQuery, useUpdatePostMutation } from './todo.service'
 import { Post } from './todo.type'
 
 const initialState: Omit<Post, 'id'> = {
-    userId:1,
-    title:'',
-    body:'testbody'
+    userId: 1,
+    title: '',
+    body: 'testbody'
 }
 
 function TodoList() {
-    const [showInput, setShowInput]= useState<Boolean>(false)
+    const [showInput, setShowInput] = useState<Boolean>(false)
     const [formData, setFormData] = useState<Omit<Post, 'id'> | Post>(initialState)
-    const [modeEdit,setModeEdit] = useState<Boolean>(false)
-    const [editPostId,setEditPostId]= useState<number>(0)
+    const [modeEdit, setModeEdit] = useState<Boolean>(false)
+    const [editPostId, setEditPostId] = useState<number>(0)
 
-    const handleShowInput = () =>{
+    const handleShowInput = () => {
         setShowInput(true)
         setFormData(initialState)
         setModeEdit(false)
     }
 
-    const {data,isLoading,isFetching}= useGetPostsQuery()
+    const { data, isLoading, isFetching } = useGetPostsQuery("posts")
 
-    const [addPost,addPostResult] = useAddPostMutation()
+    const [addPost, addPostResult] = useAddPostMutation()
 
     const [updatePost, updatePostResult] = useUpdatePostMutation()
 
     const [deletePost, deletePostResult] = useDeletePostMutation()
 
-    const handlOnchange = (e:React.ChangeEvent<HTMLInputElement>) =>{
-        setFormData((prev)=>(
+    const handlOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prev) => (
             {
                 ...prev,
                 title: e.target.value
@@ -41,70 +40,68 @@ function TodoList() {
         ))
     }
 
-    const handleEdit = (post: Post)=>{
+    const handleEdit = (post: Post) => {
         setModeEdit(true)
         setShowInput(true)
         setFormData(post)
         setEditPostId(post.id)
-        
+
     }
 
-    const handleSubmit = async()=>{
-        if(!modeEdit) {
-            const result =   await addPost(formData).unwrap()
-            console.log("you create post succes:",result)
-        }else{
-           
-            const result =   await updatePost({
-                body:formData,
-                id:editPostId
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!modeEdit) {
+            const result = await addPost(formData).unwrap()
+            console.log("you create post succes:", result)
+        } else {
+
+            const result = await updatePost({
+                body: formData,
+                id: editPostId
             }).unwrap()
 
             console.log(result);
-            
+
         }
-       
+
         setFormData(initialState)
-       
+
     }
 
-    const handleDelete = (id:number)=>{
+    const handleDelete = (id: number) => {
 
         deletePost(id)
     }
-   
-    
+
+
     return <div className="container">
-       <div className='wrap-todo'>
+        <div className='wrap-todo'>
             <h1>TODO LIST</h1>
             <div className='btn'>
                 <button className='btn-task' onClick={handleShowInput}>Add Task</button>
-                <div className='dropdown'>
-                    <span>All</span>
-                    <img src={imageDown} alt="" />
-                </div>
+
             </div>
-            {showInput && 
-             <div className='input'>
-                <input type="text" className='input-add' value={formData.title} placeholder='Enter task' onChange={handlOnchange}/>
-                <button className='btn-add' onClick={handleSubmit}>{modeEdit ? "edit" : "add"}</button>
-            </div>
+            {showInput &&
+                <form className='input' onSubmit={handleSubmit}>
+                    <input type="text" className='input-add' value={formData.title} placeholder='Enter task' onChange={handlOnchange} required />
+                    <button className='btn-add' type='submit' >{modeEdit ? "edit" : "add"}</button>
+                </form>
             }
-            
+
             <div className='content'>
-                {data && data.map((item)=>{
+                {data && data.map((item) => {
                     return <div className='item' key={item.id}>
-                    <span className="title">{item.title}</span>
-                    <div className='btn-control'>
-                        <img src={imageDelete} alt="" onClick={()=>handleDelete(item.id)}/>
-                        <img src={imageEdit} alt="" onClick={()=>handleEdit(item)}/>
+                        <span className="title">{item.title}</span>
+                        <div className='btn-control'>
+                            <img src={imageDelete} alt="" onClick={() => handleDelete(item.id)} />
+                            <img src={imageEdit} alt="" onClick={() => handleEdit(item)} />
+                        </div>
                     </div>
-                </div>
                 })}
-            
-               
+
+
             </div>
-       </div>
+        </div>
     </div>
 }
 
--- src/pages/todo.service.ts
+++ src/pages/todo.service.ts
@@ -1,76 +1,81 @@
-import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Post } from './todo.type'
 
 export const todoApi = createApi({
     reducerPath: 'blogAppi',
-    tagTypes:['Posts'],
-    baseQuery: fetchBaseQuery({baseUrl:'https://jsonplaceholder.typicode.com/'}),
-    endpoints: build =>({
-        getPosts: build.query<Post[],void>({
-            query:()=>'posts', // method khong co argument
-            providesTags(result){
-                if(result){
-                    const final =[
-                        ...result.map(({id})=>({
-                            type: 'Posts' as const,id
+    tagTypes: ['Posts'],
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+    endpoints: build => ({
+        getPosts: build.query<Post[], string>({
+            query: (endPoint) => {
+                return {
+                    url: `${endPoint}`,
+                    method: 'GET'
+                }
+            }, // method khong co argument
+            providesTags(result) {
+                if (result) {
+                    const final = [
+                        ...result.map(({ id }) => ({
+                            type: 'Posts' as const, id
                         })),
-                        {type: 'Posts' as const,id: 'LIST'}
+                        { type: 'Posts' as const, id: 'LIST' }
                     ]
                     return final
                 }
-                const final = [{type: 'Posts' as const,id :'LIST'}]
+                const final = [{ type: 'Posts' as const, id: 'LIST' }]
                 return final
             }
         }),
         /* chung ta dung mutation doi voi cac truong hop POST, PUT, DELETE
         Post la reponse tra ve va Omit<Post, 'id'> la body gui len
     */
-        addPost: build.mutation<Post,Omit<Post, 'id'>>({
-            query:(body)=>{
-                return{
+        addPost: build.mutation<Post, Omit<Post, 'id'>>({
+            query: (body) => {
+                return {
                     url: 'posts',
                     method: 'POST',
                     body,
                     headers: {
                         'Content-type': 'application/json; charset=UTF-8',
-                      },
+                    },
                 }
             },
-            invalidatesTags:(result, error, body)=>[{type: "Posts",id : 'LIST'}]
+            invalidatesTags: (result, error, body) => [{ type: "Posts", id: 'LIST' }]
         }),
 
-        updatePost: build.mutation<Post,{id:number, body:Omit<Post, 'id'>}>({
-            query:(data)=>{
+        updatePost: build.mutation<Post, { id: number, body: Omit<Post, 'id'> }>({
+            query: (data) => {
                 return {
                     url: `posts/${data.id}`,
                     method: 'PUT',
-                    body:data.body,
+                    body: data.body,
                     headers: {
                         'Content-type': 'application/json; charset=UTF-8',
-                      },
+                    },
                 }
             },
-            invalidatesTags:(result, error, data)=>[{type: "Posts",id : data.id}]
+            invalidatesTags: (result, error, data) => [{ type: "Posts", id: data.id }]
         }),
 
-        deletePost: build.mutation<{},number>({
-            query:(id)=>{
-                return{
+        deletePost: build.mutation<{}, number>({
+            query: (id) => {
+                return {
                     url: `posts/${id}`,
                     method: 'DELETE',
                     headers: {
                         'Content-type': 'application/json; charset=UTF-8',
-                      },
+                    },
                 }
             },
-            invalidatesTags:(result, error, id)=>[{type: "Posts",id : id}]
+            invalidatesTags: (result, error, id) => [{ type: "Posts", id: id }]
         })
     }),
 
-    
-    
 
-    
+
+
+
 })
 
-export const {useGetPostsQuery, useAddPostMutation, useUpdatePostMutation,useDeletePostMutation} = todoApi
+export const { useGetPostsQuery, useAddPostMutation, useUpdatePostMutation, useDeletePostMutation } = todoApi
